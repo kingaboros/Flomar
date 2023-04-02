5225cc8d9542edeb2ed3d077fd1ddb22b3a80809
KB <kinga.boros20@gmail.com>
2023-04-02 11:24:02 +0300

    projects screen

---
+++ js/openModal.js
@@ -0,0 +1,28 @@
+// const openModalBtns = document.querySelectorAll('[data-modal-target]');
+// const closeModalBtns = document.querySelectorAll('[data-close-button]');
+
+// openModalBtns.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget);
+//     openModal(modal);
+//   });
+// });
+
+// closeModalBtns.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.projectModal');
+//     closeModal(modal);
+//   });
+// });
+
+// function openModal(modal) {
+//   if (modal == null) return;
+//   modal.classList.add('active');
+//   overlay.classList.add('active');
+// }
+
+// function closeModal(modal) {
+//   if (modal == null) return;
+//   modal.classList.remove('active');
+//   overlay.classList.remove('active');
+// }
